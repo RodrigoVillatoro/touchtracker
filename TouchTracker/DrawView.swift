667962c37001ebe8ee9a043c263441eabb08e7bc
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-03-11 05:42:35 +0100

    First commit

---
+++ TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Rodrigo Villatoro on 3/11/16.
+//  Copyright © 2016 Rodrigo Villatoro. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Rodrigo Villatoro on 3/11/16.
+//  Copyright © 2016 Rodrigo Villatoro. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
